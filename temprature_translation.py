9f45017cec6b4c9707a10a07166575d37cdae228
flyinNet1020 <63777174+flyinNet1020@users.noreply.github.com>
2021-06-14 00:27:32 +0800

    Update temprature_translation.py

--- temprature_translation.py
+++ temprature_translation.py
@@ -1,5 +1,5 @@
 #translate fahrenheit to celsius
 celsius_temp = input ('Please enter the celsius temperature: ')
 celsius_temp = float(celsius_temp)
-fahren_temp = celsius_temp * 9 / 5
+fahren_temp = celsius_temp * 9 / 5 + 32
 print('fahrenheit temperature is ', fahren_temp, '℉.')
